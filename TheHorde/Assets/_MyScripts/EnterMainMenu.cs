48cc2d88fc48c2e82fa888d008c5c6fe43d4928f
thelifeinro <d_pirvan@yahoo.com>
2019-03-13 23:20:31 +0200

    Added main menu w/ navigation
    made animations for menu buttons

---
+++ TheHorde/Assets/_MyScripts/EnterMainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EnterMainMenu : MonoBehaviour
+{
+    public Animator camAnimator;
+    public GameObject uiHelpText;
+    public Button contBtn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        contBtn.interactable = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            camAnimator.SetBool("Entered", true);
+            this.enabled = false;
+            uiHelpText.SetActive(false);
+            contBtn.interactable = true;
+        }
+    }
+
+    public void Debugg()
+    {
+        Debug.Log("pressed");
+    }
+
+    //PROVIZORIU
+    public void LoadALevel()
+    {
+        SceneManager.LoadScene("TheWoods_Terrain", LoadSceneMode.Single);
+    }
+}
