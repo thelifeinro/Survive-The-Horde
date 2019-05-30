003192b42fd2e02b12f43832b96fda57848bd70e
thelifeinro <d_pirvan@yahoo.com>
2019-05-30 22:20:18 +0300

    New Levels + Trap mechanism
    30/05/19 _ STARTED 10:20
    	NEW IMPLEMENTATION:
    	-> Added Deadly Obstacles (Traps) mechanism (zombies walk inside and die);
    
    	NEW LEVEL DESIGNS:
    	->Level 4: The Woods II -> Add Crawlers; _DONE
    	->Level 5: The Military II -> Add drones _DONE
    	->Level 7: The Woods III -> Add Crawlers and Drones _ DONE
    	->Level 8: The Military III -> Add Giants to everything _DONE

---
+++ TheHorde/Assets/_MyScripts/FireLight.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireLight : MonoBehaviour
+{
+
+    private float totalSeconds;     // The total of seconds the flash wil last
+    public float maxTime;
+    public float maxBetweenTime;
+    public float maxIntensity;     // The maximum intensity the flash will reach
+    public Light myLight;        // Your light
+    private bool isFlashing = false;
+    public GameObject emission;
+    
+
+    public void Update()
+    {
+        if (!isFlashing)
+            StartCoroutine(flashNow(Random.Range(0, maxTime)));
+    }
+    public IEnumerator flashNow(float seconds)
+    {
+        isFlashing = true;
+        totalSeconds = seconds;
+        float waitTime = totalSeconds / 2;
+        // Get half of the seconds (One half to get brighter and one to get darker)
+        while (myLight.intensity < maxIntensity)
+        {
+            if(emission!=null)
+                emission.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+            myLight.intensity += Time.deltaTime / waitTime;        // Increase intensity
+            yield return null;
+        }
+        while (myLight.intensity > 0)
+        {
+            if (emission != null)
+                emission.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+            myLight.intensity -= Time.deltaTime / waitTime;
+            yield return null;
+        }
+        yield return new WaitForSeconds(Random.Range(0, maxBetweenTime));
+        isFlashing = false;
+        yield return null;
+    }
+}
